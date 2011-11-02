3f6a88920c956ce9412db3fa958ba1346650b22c
Sprenger120 <sprenger120@gmx.de>
2011-11-02 16:16:25 +0100

    +Main starts player threads and NetworkThread now

--- FastCraft-MinecraftSMPServer/main.cpp
+++ FastCraft-MinecraftSMPServer/main.cpp
@@ -14,32 +14,57 @@ GNU General Public License for more details.
 */
 
 #include <iostream>
+#include <string>
 
-#include <Poco\Thread.h>
-#include <Poco\ThreadPool.h>
-#include <Poco\Net\ServerSocket.h>
+#include <Poco/Thread.h>
+#include <Poco/ThreadPool.h>
+#include <Poco/Net/ServerSocket.h>
+#include <Poco/NumberFormatter.h>
 
 #include "NetworkHandler.h"
 #include "SettingsHandler.h"
+#include "PlayerThread.h"
 
 using std::cout;
 using std::string;
 using std::endl;
+using Poco::Thread;
+
 int main() {
+	int x;
 	SettingsHandler Settings;
-	
+
 	//Informations
-	cout<<"--- FAST CRAFT v. "+ Settings.getFastCraftVersion() + " for Minecraft " + Settings.getSupportedMCVersion() + " ---"<<"\n";
-	cout<<"Running on *:";
-	cout<<Settings.getPort();
-	cout<<" with ";
-	cout<<Settings.getMaxClients();
-	cout<<" slots"<<endl;
+	cout<<"--- FAST CRAFT v. "<<Settings.getFastCraftVersion()<<" for Minecraft "<<Settings.getSupportedMCVersion()<<" ---"<<"\n";
+	cout<<"Running on *:"<<Settings.getPort()<<" with "<<Settings.getMaxClients()<<" player slots"<<endl;
+
+	//Player Threads
+	PlayerThread* aPlayers = new PlayerThread[Settings.getMaxClients()];
+	Poco::ThreadPool PlayerThreadPool(1,Settings.getMaxClients());
+
+	for (x=0;x<=Settings.getMaxClients()-1;x++) {
+		PlayerThreadPool.defaultPool().start(aPlayers[x]);
+		
+		//Wait for thread become ready
+		while (! aPlayers[x].Ready() ) {}
+	}
+
+
+	//Start Network Thread
+	NetworkHandler NetworkHandler(aPlayers,&Settings);
+	Thread threadNetworkHandler("NetworkHandler");
+
+	threadNetworkHandler.start(NetworkHandler);
+
+	while(! NetworkHandler.Ready()) {}
+
+	cout<<"Ready"<<endl;
 
 
 
 
-	
+	//Release Player Objects
+	delete [] aPlayers;	
 
 	return 1;
 }
