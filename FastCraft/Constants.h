f1426e73fb77b87d151a69f0ce48f908e7fda41f
Sprenger120 <sprenger120@gmx.de>
2011-11-03 17:10:00 +0100

    +Add file for FastCraft constants

---
+++ FastCraft/Constants.h
@@ -0,0 +1,22 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_CONSTANTS
+#define _FASTCRAFTHEADER_CONSTANTS
+
+#define FC_JOB_CLOSECONN			1
+
+
+#endif
