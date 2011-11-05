4927dcca09b1e15d59eae564d1a0d7e5eb1b031a
Sprenger120 <sprenger120@gmx.de>
2011-11-05 14:20:25 +0100

    +Add class for entity id generation

---
+++ FastCraft/EntityProvider.h
@@ -0,0 +1,39 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_ENTITYPROVIDER
+#define _FASTCRAFTHEADER_ENTITYPROVIDER
+
+#include <vector>
+#include "Structs.h"
+using std::vector;
+
+class EntityProvider {
+private:
+	vector<EntityType> _vEntitys;
+	int _iCount;
+public:
+	//De- / constructor
+	EntityProvider();
+	~EntityProvider();
+
+	int Add(char); //Adds a new entity to the list and returns its EID
+	void Remove(int); //Delete entity from list
+
+	char getType(int); //returns type of EID
+};
+
+
+#endif
