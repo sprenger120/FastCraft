3a4d31d220605ddd9b38caa7404088900f96ae7a
Sprenger120 <sprenger120@gmx.de>
2011-11-11 15:41:17 +0100

    +Add class for easy player thread assignment

---
+++ FastCraft/PlayerPool.h
@@ -0,0 +1,45 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+/*
+* This class handles player thread assignment and slot management.
+*/
+
+#ifndef _FASTCRAFTHEADER_PLAYERPOOL
+#define _FASTCRAFTHEADER_PLAYERPOOL
+#include <iostream>
+#include <vector>
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/ThreadPool.h>
+#include "PlayerThread.h"
+#include "SettingsHandler.h"
+#include "EntityProvider.h"
+
+using std::vector;
+
+class PlayerPool {
+private:
+	EntityProvider _EntityProvider;
+	Poco::ThreadPool _ThreadPool;
+	vector<PlayerThread*> _vPlayerThreads;
+public:
+	PlayerPool(SettingsHandler*); //Constructor
+	~PlayerPool(); //Destructor
+	
+	bool isAnySlotFree(); //Returns true if there is any free slot
+	void Assign(Poco::Net::StreamSocket&); //Assigns a connection to a free thread
+private:
+	int getFreeSlot(); //Returns -1 if there is no free slot
+};
+#endif
---
+++ FastCraft/PlyerPool.cpp
@@ -0,0 +1,71 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#include "PlayerPool.h"
+#include <Poco/Thread.h>
+
+PlayerPool::PlayerPool(SettingsHandler* pSettingsHandler):
+_vPlayerThreads(0),
+_ThreadPool("PlayerThreads",1,pSettingsHandler->getMaxClients()),
+_EntityProvider()
+{
+	int iSlotCount = pSettingsHandler->getMaxClients();
+
+	_vPlayerThreads.resize(iSlotCount); //Resize to slot count
+
+	//Create Threads
+	for (int x=0;x<=_vPlayerThreads.size()-1;x++) {
+		_vPlayerThreads[x] = new PlayerThread(pSettingsHandler,&_EntityProvider);
+
+		_ThreadPool.defaultPool().start(*_vPlayerThreads[x]);
+	}
+}
+
+PlayerPool::~PlayerPool() {
+	_ThreadPool.stopAll(); //Stop all threads
+
+	for (int x=0;x<=_vPlayerThreads.size()-1;x++) { //Release objects
+		delete _vPlayerThreads[x];
+	}
+}
+
+
+bool PlayerPool::isAnySlotFree() {
+	if (getFreeSlot() == -1) {
+		return false;
+	}else{
+		return true;
+	}
+}
+
+void PlayerPool::Assign(Poco::Net::StreamSocket& StrmSocket) {
+	int iSlot;
+	
+	iSlot = getFreeSlot();
+	if (iSlot == -1) {return;}
+
+	_vPlayerThreads[iSlot]->Connect(StrmSocket);
+}
+
+
+int PlayerPool::getFreeSlot() {
+	for (int x=0;x<=_vPlayerThreads.size()-1;x++) { //Release objects
+		if (!_vPlayerThreads[x]->isAssigned()) {
+		return x;
+		}
+	}
+	return -1;
+}
+
