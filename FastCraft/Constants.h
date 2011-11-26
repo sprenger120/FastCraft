6b25d8319dd6397bcf26705a57839590365dbfca
Sprenger120 <sprenger120@gmx.de>
2011-11-26 21:14:52 +0100

    Time becomes incremented smoother now

--- FastCraft/Constants.h
+++ FastCraft/Constants.h
@@ -43,8 +43,8 @@ GNU General Public License for more details.
 #define FC_DIFFICULTY_HARD				3
 
 //Interval
-#define FC_INTERVAL_TIMESEND			500
-#define FC_INTERVAL_KEEPACTIVE			1100
+#define FC_INTERVAL_TIMESEND			20
+#define FC_INTERVAL_KEEPACTIVE			400
 
 //Map Constants
 #define FC_CHUNK_BLOCKCOUNT				32768
