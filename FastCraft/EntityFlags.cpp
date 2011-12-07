f9d51ec7897a714588321a6ccef588f4cac0b1c1
Sprenger120 <sprenger120@gmx.de>
2011-12-07 15:18:22 +0100

    Added entity flags container class

---
+++ FastCraft/EntityFlags.cpp
@@ -0,0 +1,74 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+#include "EntityFlags.h"
+
+EntityFlags::EntityFlags() {
+	clear();
+}
+
+EntityFlags::~EntityFlags() {
+}
+
+
+void EntityFlags::clear() {
+	_OnFire = false;
+	_Crouched = false;
+	_Riding = false;
+	_Sprinting = false;
+	_Eating = false;
+}
+
+bool EntityFlags::isOnFire() {
+	return _OnFire;
+}
+
+
+bool EntityFlags::isCrouched() {
+	return _Crouched;
+}
+
+
+bool EntityFlags::isRiding() {
+	return _Riding;
+}
+
+
+bool EntityFlags::isSprinting() {
+	return _Sprinting;
+}
+
+bool EntityFlags::isEating() {
+	return _Eating;
+}
+
+void EntityFlags::setOnFire(bool f) {
+	_OnFire = f;
+}
+
+void EntityFlags::setCrouched(bool f) {
+	_Crouched = f;
+}
+
+void EntityFlags::setRiding(bool f) {
+	_Riding = f;
+}
+
+void EntityFlags::setSprinting(bool f) {
+	_Sprinting = f;
+}
+
+void EntityFlags::setEating(bool f) {
+	_Eating = f;
+}
---
+++ FastCraft/EntityFlags.h
@@ -0,0 +1,44 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_ENTITYFLAGS	
+#define _FASTCRAFTHEADER_ENTITYFLAGS
+
+class EntityFlags {
+private:
+	bool _OnFire;
+	bool _Crouched;
+	bool _Riding;
+	bool _Sprinting;
+	bool _Eating;
+public:
+	EntityFlags();
+	~EntityFlags();
+
+	void clear();
+
+	bool isOnFire();
+	bool isCrouched();
+	bool isRiding();
+	bool isSprinting();
+	bool isEating();
+
+	void setOnFire(bool);
+	void setCrouched(bool);
+	void setRiding(bool);
+	void setSprinting(bool);
+	void setEating(bool);
+};
+#endif
