7bf10b82bd91ef4d464363c8ce182da9eb7d6ab0
Sprenger120 <sprenger120@gmx.de>
2011-12-07 19:56:21 +0100

    Delete unnecessary closing §  fix #29

--- FastCraft/PlayerThread.cpp
+++ FastCraft/PlayerThread.cpp
@@ -246,7 +246,6 @@ void PlayerThread::Kick(string sReason) {
 	NetworkIO::packString(_sTemp,sReason);
 	_Connection.sendBytes(_sTemp.c_str(),_sTemp.length());
 
-//	Thread::sleep(100);
 	if (isSpawned()) { // If names is known
 		cout<<_sName<<" was kicked for: "<<sReason<<"\n"; 
 	}
@@ -259,7 +258,6 @@ void PlayerThread::Kick() {
 	_sTemp.append<char>(2,0x0);
 	_Connection.sendBytes(_sTemp.c_str(),_sTemp.length());
 
-	//Thread::sleep(100);
 	if (isSpawned()) { // If names is known
 		cout<<_sName<<" was kicked"<<"\n"; 
 	}
@@ -628,7 +626,6 @@ void PlayerThread::Packet254_ServerListPing() {
 	Poco::NumberFormatter::append(_sTemp,_PlayerCount); //Player count
 	_sTemp.append("§");
 	Poco::NumberFormatter::append(_sTemp,SettingsHandler::getPlayerSlotCount()); //player slots
-	_sTemp.append("§");
 
 	Kick(_sTemp);
 }
