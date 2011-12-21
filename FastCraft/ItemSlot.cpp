b1f553deb4cfa1c44ef433363686c4ab846de7f6
Sprenger120 <sprenger120@gmx.de>
2011-12-21 19:06:09 +0100

    Added slot data container class

---
+++ FastCraft/ItemSlot.cpp
@@ -0,0 +1,265 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#include "ItemSlot.h"
+#include "ItemInfoStorage.h"
+#include <Poco/Exception.h>
+#include "NetworkIO.h"
+
+ItemSlot::ItemSlot(): 
+_vEnchantments(0)
+{
+	_iItemID = 0;
+	_iStackSize = 0;
+	_iUsage = 0;
+	_isTool = false;
+}
+
+ItemSlot::ItemSlot(short id,char stacksize):
+_vEnchantments(0)
+{
+	char iInfo;
+	try{
+		iInfo = ItemInfoStorage::getMaxStackSize(id);
+	}catch (Poco::RuntimeException) {
+		std::cout<<"ItemSlot::ItemSlot Item wasn't found. Affected:"<<id<<"\n";
+		clear();
+		return;
+	}
+
+	_iItemID = id;
+	_iUsage = 0;
+
+	//Check stack size
+	if (stacksize<0){
+		_iStackSize=0;
+		std::cout<<"ItemSlot::ItemSlot Stacksize invalid. Affected:"<<id<<"\n";
+	}
+	if (stacksize > iInfo) {
+		_iStackSize = iInfo;
+		std::cout<<"ItemSlot::ItemSlot Stacksize exceeded. Affected:"<<id<<"\n";
+	}else{
+		_iStackSize = stacksize;
+	}
+	
+	//Set isTool
+	if (ItemInfoStorage::isDamageable(id)) {
+		_isTool=true;
+	}else{
+		_isTool=false;
+	}
+}
+
+ItemSlot::ItemSlot(short id,char stacksize,short usage) :
+_vEnchantments(0)
+{
+	short iUsageInfo;
+	char iStackInfo;
+
+	try	{
+		iStackInfo = ItemInfoStorage::getMaxStackSize(id);
+		iUsageInfo = ItemInfoStorage::getMaxUsage(id);
+	}catch (Poco::RuntimeException) {
+		std::cout<<"ItemSlot::ItemSlot(ex) Item wasn't found. Affected:"<<id<<"\n";
+		clear();
+		return;
+	}
+
+	_iItemID = id;
+
+	//Check stack size
+	if (stacksize<0){
+		_iStackSize=0;
+		std::cout<<"ItemSlot::ItemSlot(ex) Stacksize invalid. Affected:"<<id<<"\n";
+	}
+	if (stacksize > iStackInfo) {
+		_iStackSize = iStackInfo;
+		std::cout<<"ItemSlot::ItemSlot(ex) Stacksize exceeded. Affected:"<<id<<"\n";
+	}else{
+		_iStackSize = stacksize;
+	}
+
+	//Check usage
+	if (usage > iUsageInfo || usage < 0) {
+		std::cout<<"ItemSlot::ItemSlot(ex) Usage exceeded/invalid. Affected:"<<id<<"\n";
+		_iUsage = 0;
+	}else{
+		_iUsage = usage;
+	}
+	
+	//Set isTool
+	if (ItemInfoStorage::isDamageable(id)) {
+		_isTool=true;
+	}else{
+		_isTool=false;
+	}
+}
+
+ItemSlot::ItemSlot(NetworkIO& rNetwork) {
+	try {
+		readFromNetwork(rNetwork);
+	} catch(Poco::RuntimeException& ex) {
+		ex.rethrow();
+	}
+}
+
+ItemSlot::~ItemSlot() {
+	_vEnchantments.clear();
+}
+
+short ItemSlot::getItemID() {
+	return _iItemID;
+}
+
+void ItemSlot::setItemID(short id) {
+	try{
+		if (ItemInfoStorage::isDamageable(id)) {
+			_isTool=true;
+		}else{
+			_isTool=false;
+		}
+	}catch(Poco::RuntimeException) {
+		std::cout<<"ItemSlot::setItemID Item wasn't found. Affected:"<<id<<"\n";
+		throw Poco::RuntimeException("ID not registered");
+	}
+	_iItemID = id;
+}
+
+char ItemSlot::getStackSize() {
+	return _iStackSize;
+}
+
+void ItemSlot::setStackSize(char size) {
+	char SizeInfo;
+
+	
+	if (size<0){
+		_iStackSize=0;
+		std::cout<<"ItemSlot::setStackSize Stacksize invalid."<<"\n";
+		return;
+	}
+
+	SizeInfo = ItemInfoStorage::getMaxStackSize(_iItemID);
+
+	if (size > SizeInfo) {
+		_iStackSize = SizeInfo;
+		std::cout<<"ItemSlot::setStackSize Stacksize exceeded."<<"\n";
+	}else{
+		_iStackSize = size;
+	}
+}
+
+short ItemSlot::getUsage() {
+	return _iUsage;
+}
+
+
+void ItemSlot::IncrementUsage() {
+	if (!_isTool) {
+		std::cout<<"ItemSlot::IncrementUsage Not a tool!"<<"\n";
+		throw Poco::RuntimeException("Not a tool");
+	}
+	short iUsageInfo = ItemInfoStorage::getMaxUsage(_iItemID);
+
+	if(_iUsage > iUsageInfo || _iUsage < 0) {
+		if (_iStackSize == 1) {//Last item in stack 
+			clear();
+		}else{//Decrement stack size if there is more than one
+			_iStackSize--;
+			_iUsage = 0;
+		}
+		return;
+	}
+	_iUsage++;
+}
+
+void ItemSlot::setUsage(short iUsage){
+	short iUsageInfo;	
+	if (!_isTool) {
+		std::cout<<"ItemSlot::IncrementUsage Not a tool!"<<"\n";
+		throw Poco::RuntimeException("Not a tool");
+	}
+
+	iUsageInfo = ItemInfoStorage::getMaxUsage(_iItemID);
+
+	if(iUsage > iUsageInfo || iUsage < 0) {
+		//Decrement stack size if more than one
+		if (_iStackSize == 1) {
+			clear(); //Stack will is empty
+		}else{
+			_iStackSize--;
+			_iUsage = 0;
+		}
+		return;
+	}
+	_iUsage = iUsage;
+}
+
+void ItemSlot::clear() {
+	_iItemID = 0;
+	_iStackSize = 0;
+	_iUsage = 0;
+	_isTool=false;
+}
+
+bool ItemSlot::isEmpty() {
+	if (_iStackSize == 0) {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+void ItemSlot::readFromNetwork(NetworkIO& rNetwork) {
+	try {
+	_iItemID = rNetwork.readShort();
+	if (_iItemID == -1) { //Slot is empty
+		clear();
+		return;
+	}
+
+	_isTool=false;
+	_iStackSize = rNetwork.readByte();
+	_iUsage = rNetwork.readShort();
+
+	short iUsageInfo;
+	char iStackInfo;
+	try	{
+		iStackInfo = ItemInfoStorage::getMaxStackSize(_iItemID);
+		iUsageInfo = ItemInfoStorage::getMaxUsage(_iItemID);
+	}catch (Poco::RuntimeException) {
+		std::cout<<"ItemSlot::ItemSlot(fetch) Item wasn't found. Affected:"<<_iItemID<<"\n";
+		clear();
+		return;
+	}
+
+	//Check values
+	if (_iStackSize > iStackInfo) {
+		_iStackSize = iStackInfo;
+		std::cout<<"ItemSlot::ItemSlot(fetch) Stacksize exceeded. Affected:"<<_iItemID<<"\n";
+	}
+	if(_iUsage > iUsageInfo || _iUsage < 0) {
+		_iUsage = 0;
+	}
+
+	//Check if item has a damage bar
+	if (ItemInfoStorage::isDamageable(_iItemID)) {
+		_isTool=true;
+		std::cout<<"NBT Playload:"<< rNetwork.readShort()<<"\n";
+	}
+	}catch(Poco::RuntimeException& ex) {
+		ex.rethrow();
+	}
+}
---
+++ FastCraft/ItemSlot.h
@@ -0,0 +1,186 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+
+#ifndef _FASTCRAFTHEADER_ITEMSLOT
+#define _FASTCRAFTHEADER_ITEMSLOT	
+#include <vector>
+#include "Structs.h"
+
+using std::vector;
+class NetworkIO;
+
+/*
+Class Note: If you change something with set... functions, it won't be sync with client
+			call synchronizeInventory from PlayerInventory class for this
+Note2: Enchantments are not supported right now
+*/
+
+class ItemSlot {
+private:
+	short _iItemID;
+	char _iStackSize;
+	short _iUsage;
+	bool _isTool;
+
+	vector<Enchantment> _vEnchantments;
+public:
+	/*
+	* Construct as a free slot
+	*/
+	ItemSlot();
+
+
+	/*
+	* Constructs as a used slot
+	* Items with a usagebar will be used 
+
+	Parameter:
+	@1 : ItemID
+	@2 : Stack size
+	*/
+	ItemSlot(short,char);
+
+	/*
+	* Construct as a used slot with extra informations
+
+	Parameter: 
+	@1 : ItemID
+	@2 : Stack size
+	@3 : Usage
+	*/
+	ItemSlot(short,char,short);
+
+	/*
+	* Construct with a NetworkIO reference and reads slot data from given TCP stream
+	* Will throw Poco::RuntimeException, if connection gets aborded
+
+	Parameter:
+	@1 : NetworkIO reference
+	*/
+	ItemSlot(NetworkIO&);
+
+
+	/*
+	* Destructor
+	*/
+	~ItemSlot();
+
+
+	/*
+	* Adds an enchantment to item slot
+	* Will throw a Poco::RuntimeException if slot contains more than one item (itemstack) or an unenchantable item
+
+	Parameter:
+	@1 : Reference to a Enchantment struct
+	*/
+	//void addEnchantment(Enchantment&);
+
+	
+	/*
+	* Removes a enchantment
+	* Will throw a Poco::RuntimeException if array index is out of bound
+
+	Parameter:
+	@1 : ID of enchantment
+	*/
+	//void removeEnchantment(int);
+
+
+	/*
+	* Returns a vector of all enchantments
+	* Will return a blank vector if there a no enchantments on item or item is not enchantable
+	*/
+	//vector<Enchantment> getEnchantments();
+
+
+	/*
+	* Returns item ID
+	*/
+	short getItemID();
+
+
+	/*
+	* Sets item ID
+	* will throw Poco::RuntimeException, if item doesn't exists
+
+	Parameter:
+	@1 : new item ID
+	*/
+	void setItemID(short);
+
+
+	/*
+	* Returns acutal stack size
+	*/
+	char getStackSize();
+
+
+	/*
+	* Sets size of item stack
+	* Will set stack size to orginal maximal stack size if out of bound
+	*/
+	void setStackSize(char);
+
+
+	/* 
+	* Returns actual tool usage
+	* Will return -1 if item hasn't have a usagebar
+	*/
+	short getUsage();
+
+
+	/*
+	* Increments usage by one
+	* Will clear item slot/decrease stack if usage of this item is exceeded
+	* throws Poco::RuntimeException if item hasn't have a usagebar 
+	*/
+	void IncrementUsage();
+
+
+	/*
+	* Sets actual usage
+	* If usage is bigger than maximal orginal item usage or lower than 0, the slot will be cleared / stack decreased
+	* throws Poco::RuntimeException if item hasn't have a usagebar 
+
+	Parameter:
+	@1 : Usage count
+	*/
+	void setUsage(short);
+
+
+	/*
+	* Clears actual item slot
+	*/
+	void clear();
+
+
+	/*
+	* Returns true if slot is empty, false otherwise
+	*/
+	bool isEmpty();
+
+
+	/*
+	* Reads slot information from NetworkIO
+	* Will throw Poco::RuntimeException, if connection gets aborded
+	* Will overwrite existing data
+
+	Parameter:
+	@1 : Reference to a vaild NetworkIO instance
+	*/
+	void readFromNetwork(NetworkIO&);
+};
+#endif
