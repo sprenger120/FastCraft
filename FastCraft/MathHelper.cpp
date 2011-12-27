989d643f297aadc558af0bbab17d7146cb5cf199
Sprenger120 <sprenger120@gmx.de>
2011-12-27 17:51:18 +0100

    Added class for mathematical calculations

---
+++ FastCraft/MathHelper.cpp
@@ -0,0 +1,51 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+#include "MathHelper.h"
+#include <cmath>
+
+MathHelper::MathHelper() {
+}
+
+MathHelper::~MathHelper() {
+}
+
+double MathHelper::distance2D(double x1,double y1,double x2,double y2) {
+		return  sqrt (  (x1-x2) * (x1-x2) + 
+					    (y1-y2) * (y1-y2)
+				     );
+}
+
+double MathHelper::distance2D(EntityCoordinates s,EntityCoordinates t) {
+	return  sqrt (  (s.X-t.X) * (s.X-t.X) + 
+					(s.Z-t.Z) * (s.Z-t.Z)
+				 );
+}
+
+
+double MathHelper::distance3D(double x1,double y1,double z1,double x2,double y2,double z2) {
+			return  sqrt (  (x1-x2) * (x1-x2) + 
+					        (y1-y2) * (y1-y2) +
+							(z1-z2) * (z1-z2)
+				     );
+}
+
+
+
+double MathHelper::distance3D(EntityCoordinates s,EntityCoordinates t) {
+		return  sqrt (  (s.X-t.X) * (s.X-t.X) + 
+					    (s.Z-t.Z) * (s.Z-t.Z) + 
+						(s.Y-t.Y) * (s.Y-t.Y)  
+				     );
+}
---
+++ FastCraft/MathHelper.h
@@ -0,0 +1,63 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011  Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_MATHHELPER
+#define _FASTCRAFTHEADER_MATHHELPER
+#include "EntityCoordinates.h"
+
+class MathHelper {
+private:
+	MathHelper();
+	~MathHelper();
+public:
+	/*
+	* Calculates distance between two points
+	* 1 = source, 2=target
+
+	Parameter:
+	@1 : x1
+	@2 : y1
+
+	@3 : x2
+	@4 : y2
+
+	Parameter:
+	@1 : EntityCoordinates1
+	@2 : EntityCoordinates2
+	*/
+	static double distance2D(double,double,double,double);
+	static double distance2D(EntityCoordinates,EntityCoordinates);
+
+	/*
+	* Calculates distance between two points
+	* 1 = source, 2=target
+
+	Parameter:
+	@1 : x1
+	@2 : y1
+	@3 : z1
+
+	@4 : x2
+	@5 : y2
+	@6 : z2
+
+	Parameter:
+	@1 : EntityCoordinates1
+	@2 : EntityCoordinates2
+	*/
+	static double distance3D(double,double,double, double,double,double);
+	static double distance3D(EntityCoordinates,EntityCoordinates);
+};
+#endif
