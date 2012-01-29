384ec917fbf1414aebb5cb9ee92461f68eb9597e
Sprenger120 <sprenger120@gmx.de>
2012-01-29 17:41:32 +0100

    Renamed main Settings directory to Data main loads all Worlds now

--- FastCraft/main.cpp
+++ FastCraft/main.cpp
@@ -23,10 +23,11 @@ GNU General Public License for more details.
 #include "PackingThread.h"
 #include "ServerTime.h"
 #include "ItemInfoStorage.h"
+#include "Structs.h"
+#include "WorldStorage.h"
 #include <Poco/Path.h>
 #include <Poco/File.h>
 #include <Poco/Data/SQLite/Connector.h>
-
 using std::cout;
 using std::stringstream;
 using Poco::Thread;
@@ -44,78 +45,74 @@ int main(int argc, char *argv[]) {
 	Poco::Path FCPath(argv[0]); 
 	FCPath.setFileName(""); //Remove filename
 
+
 	//Main directory
-	FCPath.pushDirectory("Settings");
+	FCPath.pushDirectory("Data");
 	Poco::File settingsDirectory(FCPath.toString());
+	if (!settingsDirectory.exists()) {settingsDirectory.createDirectories();}
+
+	//Config
+	cout<<"Loading settings... "<<"\n";
+	
+	FCPath.setFileName("settings.xml");
+	SettingsHandler::readConfiguration(FCPath);
+	FCPath.setFileName("");
+	
+	cout<<"Done."<<"\n\n";
+
 
 	//Item databases
 	FCPath.pushDirectory("ItemDatabases");
 	Poco::File itemDatabasesDirectory(FCPath.toString());
-
-	if (!settingsDirectory.exists()) {
-		cout<<"Settings directory not existing. Creating..."<<"\n";
-		settingsDirectory.createDirectories();
-	}
-
-	if (!itemDatabasesDirectory.exists()) {
-		cout<<"Item databases directory not existing. Creating..."<<"\n";
-		itemDatabasesDirectory.createDirectories();
-	}
-
-
-	/*
-	* Read item databases
-	*/
+	if (!itemDatabasesDirectory.exists()) {itemDatabasesDirectory.createDirectories();}
+	
 	ItemInfoStorage::loadDatabases(FCPath);
 	cout<<"\n";
-
-
-	/*
-	* Load settings
-	*/
-	cout<<"Loading settings... "<<"\n";
-	try {
-		SettingsHandler(); //Load configuration into static variables
-	} catch (Poco::RuntimeException) {
-		cout<<"Error while loading settings"<<"\n";
-		Thread::sleep(3000);
-		return 1;
-	}
-	cout<<"Done."<<"\n\n";
 	
+	
+	//Worlds directory
+	FCPath.popDirectory();
+	FCPath.pushDirectory("Worlds");
+	Poco::File worldDirectory(FCPath.toString());
+	if (!worldDirectory.exists()) {worldDirectory.createDirectories();}
+
 
 	/*
 	* Create threads 
 	*/
 
-	//Threads
+	//Creating Threads & Classes
 	Thread threadPackingThread("Chunk Packer");
 	Thread threadPlayerPool("PlayerPool");
 	Thread threadAcceptThread("Network Acceptor");
 
-
-	//Create ChunkPacker Thread
 	PackingThread Packer;
-	threadPackingThread.start(Packer);
-
-
-	//Create player pool thread
-	PlayerPool PPool(Packer);
-	threadPlayerPool.start(PPool);
-
-
-	//Acceptor thread
+	PlayerPool PPool(Packer);	
 	AcceptThread Acceptor(PPool);
-	threadAcceptThread.start(Acceptor);
+	
+	
+	/*
+	* Loading world
+	*/
+	cout<<"Loading maps..."<<"\n";
+	WorldStorage worldStorage(PPool,FCPath);
+	worldStorage.loadAllWorlds();
+	cout<<"Done.\n\n";
 
 
 	/*
-	* All right :)
+	* Start Threads
 	*/
+	threadPackingThread.start(Packer);
+	threadPlayerPool.start(PPool);
+	threadAcceptThread.start(Acceptor);
+	
 
+	//Init Done!
 	cout<<"Running on *:"<<SettingsHandler::getPort()<<" with "<<SettingsHandler::getPlayerSlotCount()<<" player slots"<<std::endl;
 	cout<<"Ready."<<"\n"<<std::endl;
-	while (1) {		
+
+	while(1) {
 		Thread::sleep(1000);
 		ServerTime::tick();
 	}
