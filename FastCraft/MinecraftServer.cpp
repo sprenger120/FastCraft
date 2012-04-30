1675a351bdfa97ff3e130cfeb29196303230f488
Sprenger120 <sprenger120@gmx.de>
2012-05-01 00:24:40 +0200

    Added class that represents a Minecraft server

---
+++ FastCraft/MinecraftServer.cpp
@@ -0,0 +1,407 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+#include "MinecraftServer.h"
+#include <Poco/String.h>
+#include <Poco/File.h>
+#include <fstream>
+#include <Poco/Exception.h>
+#include <Poco/DOM/DOMParser.h>
+#include <Poco/SAX/InputSource.h>
+#include <Poco/XML/XMLException.h>
+#include <Poco/SAX/SAXException.h>
+#include <Poco/NumberParser.h>
+#include "Constants.h"
+#include "ItemInformationProvider.h"
+#include "World.h"
+#include "PlayerPool.h"
+#include "AcceptThread.h"
+#include <ctime>
+#include <Poco/ScopedLock.h>
+using std::cout;
+
+MinecraftServer::MinecraftServer(string sName,Poco::Path sRootPath) :
+_sServerName(""),
+	_sServerDescription		("FastCraft Minecraft server"),
+	_sMOTD					("§dWelcome to FastCraft 0.0.2 Alpha server."),
+	_sMainMapName			("main"),
+	_sServerFullMessage     ("Server is full!"),
+	_vsAdditionalWorlds		(0),
+	_vpWorlds				(0),
+	ServerThreadBase		("Minecraft Server"),
+	_iInGameTime			(0)
+{
+	_iPort						= 25565;
+	_iPlayerSlotCount			= 16;
+	_iServerMode				= Constants::get("/ServerMode/Survival");
+	_iDifficulty				= Constants::get("/Difficulty/Easy");
+	_fOnlineMode				= true;
+	_fWhitelist					= false;
+	_fAllowNeather				= false;
+	_fLoadEnd					= true;
+	_fSpawnPeacefulMobs			= true;
+	_fSpawnHostileMobs			= true;
+	_iViewDistance				= 10;
+	_fPVP						= true;
+	_fAllowFlyMod				= false;
+	_dMaxPlayerSpeed			= 7.5;
+
+	_clockCreation.start();
+	_iID = 100;
+
+	//Check name
+	if (sName.compare("") == 0) {throw Poco::RuntimeException("Illegal name");}
+
+	
+	{
+		//Read config
+		cout<<"Reading configuration...\n";
+		
+		sRootPath.setFileName("settings.xml");
+		Poco::File ckeckExistanceFile(sRootPath);
+		
+		if (!ckeckExistanceFile.exists()) {
+			cout<<"Settings file doesn't exist. Starting with standart configuration.";
+		}else if(!(ckeckExistanceFile.canRead() && ckeckExistanceFile.canWrite())){
+			cout<<"Unable to read/write settings file. Starting with standart configuration.\n";
+		}else{
+			readConfiguration(sRootPath);
+		}
+	}
+
+	{
+		//Reading items.db
+		cout<<"Reading item database...\n";
+		sRootPath.setFileName("items.db");
+		Poco::File ckeckExistanceFile(sRootPath);
+
+
+		if (!ckeckExistanceFile.exists()) {
+			cout<<"Item database doesn't exist. Aborting server startup!";
+			return;
+		}
+		if(!(ckeckExistanceFile.canRead() && ckeckExistanceFile.canWrite())){
+			cout<<"Unable to read/write item database. Aborting server startup!\n";
+			return;
+		}
+		_pItemInformationProvider = new ItemInformationProvider(sRootPath);
+	}
+	
+
+	//load words
+	_vpWorlds.push_back(new World(_sMainMapName,0,this));
+	if (!_vsAdditionalWorlds.empty()) {
+		for(int x=0;x<=_vsAdditionalWorlds.size()-1;x++) {
+			_vpWorlds.push_back(new World(_vsAdditionalWorlds[x],0,this));
+		}
+	}
+
+	//start playerpool
+	_pPlayerPool = new PlayerPool(this);
+
+
+	//start accept thread
+	try{
+		_pAcceptThread = new AcceptThread(this);
+	}catch(Poco::RuntimeException& ex) { 
+		cout<<"Unable to start server: "<< ex.message()<<"\n"<<std::endl;
+		return;
+	}
+
+	startThread(this);
+
+	cout<<sName<<" started successfully! (Port:"<<_iPort<<")\n"<<std::endl;
+}
+
+MinecraftServer::~MinecraftServer() {
+	cout<<"Shutting down server "<<_sServerName<<"...\n";
+	delete _pAcceptThread;
+	delete _pPlayerPool;
+	delete _pItemInformationProvider;
+	if (!_vpWorlds.empty()) {
+		for(int x=0;x<=_vpWorlds.size()-1;x++) {
+			delete _vpWorlds[x];
+		}
+	}
+
+	killThread();
+}
+
+
+void MinecraftServer::run() {
+	_iThreadStatus = FC_THREADSTATUS_RUNNING;
+
+	/*Poco::Stopwatch _TickClock;
+	_TickClock.start();*/
+
+
+	while(_iThreadStatus==FC_THREADSTATUS_RUNNING)  {
+		Poco::Thread::sleep(200);
+		_iInGameTime++;
+	}
+
+	_iThreadStatus = FC_THREADSTATUS_DEAD;
+}
+
+void MinecraftServer::readConfiguration(Poco::Path& path) {
+	std::ifstream Input(path.toString());
+
+	if (!Input.good()) {
+		cout<<"***Error: Configuration path invalid!"<<"\n";
+		return;
+	}
+
+	Poco::XML::InputSource ISource(Input);
+	Poco::XML::DOMParser parser;
+	Poco::AutoPtr<Poco::XML::Document> pDoc;
+
+	try {
+		pDoc = parser.parse(&ISource);
+	}catch(Poco::XML::XMLException& ex) {
+		cout<<"***Error while parsing settings: "<<ex.message()<<"\n";
+		return;
+	}
+
+
+	/*
+	* Parsing settings
+	*/
+	int iData;
+	if (!parseNodeInt(pDoc,"/settings/DocVer",iData,FC_SUPPORTED_DOCVER,FC_SUPPORTED_DOCVER)) {
+		cout<<"***Invalid settings DocVer\n";
+	}else{
+		string sTemp("");
+		if(parseNodeInt(pDoc,"/settings/Network/Port",iData,0,65535)) {_iPort = short(iData);} 
+		if(parseNodeInt(pDoc,"/settings/Network/Slots",iData,1,255)) {_iPlayerSlotCount = short(iData);}
+		parseNodeString(pDoc,"/settings/Server/Description",_sServerDescription);
+		parseNodeString(pDoc,"/settings/Server/motd",_sMOTD);
+		parseNodeString(pDoc,"/settings/Server/ServerFullMessage",_sServerFullMessage);
+		if(parseNodeInt(pDoc,"/settings/Server/OnlineMode",iData,0,1)) {_fOnlineMode = Int2Bool(iData);}
+		if(parseNodeInt(pDoc,"/settings/Server/Whitelist",iData,0,1)) {_fWhitelist = Int2Bool(iData);}
+		if(parseNodeInt(pDoc,"/settings/Server/ServerMode",iData,0,1)) {_iServerMode = Int2Bool(iData);}
+		if(parseNodeInt(pDoc,"/settings/Server/Difficulty",iData,0,1)) {_iDifficulty = (char)iData;}
+		if(parseNodeInt(pDoc,"/settings/Map/AllowNeather",iData,0,1)) {_fAllowNeather = Int2Bool(iData);}
+		if(parseNodeInt(pDoc,"/settings/Map/ViewDistance",iData,7,12)) {_iViewDistance = (char)iData;}
+		parseNodeString(pDoc,"/settings/Map/RootMap",_sMainMapName);
+		if(parseNodeInt(pDoc,"/settings/Map/loadEnd",iData,0,1)) {_fLoadEnd = Int2Bool(iData);}
+		if(parseNodeString(pDoc,"/settings/Map/AdditionalWorlds",sTemp)) {
+			if (!sTemp.empty()) {
+				string sPart("");
+				for (int x=0;x<=sTemp.size()-1;x++) {
+					if (sTemp[x] == ',') {
+						if(sPart.compare("") != 0) {
+							_vsAdditionalWorlds.push_back(sPart);
+							sPart.clear();
+							continue;
+						}
+					}else{
+							sPart.append(1,sTemp[x]);
+					}
+				}
+				if(sPart.compare("") != 0) {_vsAdditionalWorlds.push_back(sPart);}
+			}
+		}
+		if(parseNodeInt(pDoc,"/settings/Spawning/Peaceful",iData,0,1)) {_fSpawnPeacefulMobs = Int2Bool(iData);}
+		if(parseNodeInt(pDoc,"/settings/Spawning/Hostile",iData,0,1)) {_fSpawnHostileMobs = Int2Bool(iData);}
+		if(parseNodeInt(pDoc,"/settings/Player/AllowPVP",iData,0,1)) {_fPVP = Int2Bool(iData);}
+		if(parseNodeInt(pDoc,"/settings/Player/FlyMod/Allow",iData,0,1)) {_fAllowFlyMod = Int2Bool(iData);}
+		parseNodeDouble(pDoc,"/settings/Player/FlyMod/MaxSpeed",_dMaxPlayerSpeed,7.5,50.0);
+		if (!_fOnlineMode) {	
+			cout<<"***Config WARNING: Server runs in unsafe mode. Hackers can connect without verification!"<<"\n";  	
+			cout<<"***Solution: Set OnlineMode in your fastcraft.properties to true."<<"\n";
+		}
+	}
+}
+
+unsigned short MinecraftServer::getPort() {
+	return _iPort; 
+}
+
+unsigned short MinecraftServer::getPlayerSlotCount() {
+	return _iPlayerSlotCount;
+}
+
+string MinecraftServer::getServerDescription() {
+	return _sServerDescription;
+}
+
+bool MinecraftServer::getServerMode() {
+	return _iServerMode;
+}
+
+char MinecraftServer::getDifficulty() {
+	return _iDifficulty;
+}
+
+bool MinecraftServer::isOnlineModeActivated() {
+	return _fOnlineMode;
+}
+
+bool MinecraftServer::isWhitelistActivated() {
+	return _fWhitelist;
+}
+
+string MinecraftServer::getServerMOTD() {
+	return _sMOTD;
+}
+
+bool MinecraftServer::isFlyModAllowed() {
+	return _fAllowFlyMod;
+}
+
+double MinecraftServer::getMaxMovementSpeed() {
+	return _dMaxPlayerSpeed;
+}
+
+bool MinecraftServer::isNeatherAllowed() {
+	return _fAllowNeather;
+}
+
+string MinecraftServer::getMainWorldName() {
+	return _sMainMapName;
+}
+
+bool MinecraftServer::isPeacefulAnimalSpawningActivated() {
+	return _fSpawnPeacefulMobs;
+}
+
+bool MinecraftServer::isHostileAnimalSpawningActivated() {
+	return _fSpawnHostileMobs;
+}
+
+char MinecraftServer::getViewDistance() {
+	return _iViewDistance;
+}
+
+bool MinecraftServer::isPVPActivated() {
+	return _fPVP;
+}
+
+bool MinecraftServer::parseNodeInt(Poco::AutoPtr<Poco::XML::Document> pDoc,string sPath,int& rData,int iMin,int iMax) { 
+	Poco::XML::Node* pNode;
+
+	pNode = pDoc->getNodeByPath(sPath);
+
+	if(pNode==NULL) {
+		cout<<"***Error: Node "<<sPath<<" was not found!"<<"\n";
+		return false;
+	}else{
+		string sData("");
+		int iData;
+
+		sData = pNode->innerText();
+		try {
+			iData = Poco::NumberParser::parseUnsigned(sData);
+		}catch(Poco::SyntaxException) {
+			cout<<"***Error: Unable to parse data from node "<<sPath<<"\n";
+			return false;
+		}
+
+		if (iData < iMin || iData > iMax) {
+			cout<<"***Error: Value of "<<sPath<<" is invalid!\n";
+			return false;
+		}
+		rData = iData;
+		return true;
+	}
+}
+
+bool MinecraftServer::parseNodeDouble(Poco::AutoPtr<Poco::XML::Document> pDoc,string sPath,double& rData,double dMin,double dMax) { 
+	Poco::XML::Node* pNode;
+
+	pNode = pDoc->getNodeByPath(sPath);
+
+	if(pNode==NULL) {
+		cout<<"***Error: Node "<<sPath<<" was not found!"<<"\n";
+		return false;
+	}else{
+		string sData("");
+		double dData;
+
+		sData = pNode->innerText();
+		try {
+			dData = Poco::NumberParser::parseFloat(sData);
+		}catch(Poco::SyntaxException) {
+			cout<<"***Error: Unable to parse data from node "<<sPath<<"\n";
+			return false;
+		}
+
+		if (dData < dMin || dData > dMax) {
+			cout<<"***Error: Value of "<<sPath<<" is invalid!\n";
+			return false;
+		}
+		rData = dData;
+		return true;
+	}
+}
+
+bool MinecraftServer::parseNodeString(Poco::AutoPtr<Poco::XML::Document> pDoc,string sPath,string& rTarget) { 
+	Poco::XML::Node* pNode;
+	pNode = pDoc->getNodeByPath(sPath);
+
+	if (pNode == NULL) {
+		cout<<"***Error: Node "<<sPath<<" was not found!"<<"\n";
+		return false;
+	}else{
+		rTarget.assign(pNode->innerText());
+		return true;
+	}
+}
+
+bool MinecraftServer::Int2Bool(int& r) {
+	if(r){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+string MinecraftServer::getServerFullMessage() {
+	return _sServerFullMessage;
+}
+
+ItemInformationProvider* MinecraftServer::getItemInfoProvider() {
+	return _pItemInformationProvider;
+}
+
+PlayerPool* MinecraftServer::getPlayerPool() {
+	return _pPlayerPool;
+}
+
+World* MinecraftServer::getWorldByName(string sName) {
+	if (!_vpWorlds.empty()) {
+		for (int x=0;x<=_vpWorlds.size()-1;x++) {
+			if (_vpWorlds[x]->getName().compare(sName) == 0) {
+				return _vpWorlds[x];
+			}
+		}
+	}
+
+	return NULL;
+}
+
+
+Tick MinecraftServer::getInGameTime() {
+	return _iInGameTime;
+}
+
+Tick MinecraftServer::getTimestamp() {
+	return _clockCreation.elapsed() / 1000;
+}
+
+int MinecraftServer::generateID(){
+	Poco::ScopedLock<Poco::Mutex> scopeLock(_idMutex);
+	_iID++;
+	return _iID;
+}
---
+++ FastCraft/MinecraftServer.h
@@ -0,0 +1,258 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_MINECRAFTSERVER
+#define _FASTCRAFTHEADER_MINECRAFTSERVER
+#include <iostream>
+#include <Poco/Path.h>
+#include <Poco/ThreadPool.h>
+#include <vector>
+#include <Poco/AutoPtr.h>
+#include <Poco/DOM/Document.h>
+#include <Poco/Path.h>
+#include "ServerThreadBase.h"
+#include <Poco/Stopwatch.h>
+#include <Poco/Mutex.h>
+
+
+//Forward definitions
+class AcceptThread;
+class SettingsHandler;
+class PlayerPool;
+class PackingThread;
+class ItemInformationProvider;
+class PlayerPool;
+class World;
+
+using std::string;
+using std::vector;
+
+typedef unsigned long long Tick;
+
+class MinecraftServer : public ServerThreadBase {
+private:
+	/*Settings*/
+	unsigned short _iPort;
+	unsigned short _iPlayerSlotCount;
+	string _sServerDescription;
+	string _sMOTD;
+	string _sServerFullMessage;
+	bool _iServerMode;
+	char _iDifficulty;
+	bool _fOnlineMode;
+	bool _fWhitelist;
+	bool _fAllowNeather;
+	bool _fLoadEnd;
+	vector<string> _vsAdditionalWorlds;
+	string _sMainMapName;
+	bool _fSpawnPeacefulMobs;
+	bool _fSpawnHostileMobs;
+	char _iViewDistance;
+	bool _fPVP;
+	bool _fAllowFlyMod;
+	double _dMaxPlayerSpeed;
+	string _sServerName;
+
+	/* ID generation */
+	int _iID;
+	Poco::Mutex _idMutex;
+
+	/* Base classes */
+	vector<World*> _vpWorlds;
+	AcceptThread* _pAcceptThread;
+	PlayerPool* _pPlayerPool;
+	ItemInformationProvider* _pItemInformationProvider;
+
+	/* Time stuff */
+	Poco::Stopwatch _clockCreation;
+	Tick   _iInGameTime;
+public:
+	/*
+	* Constructor
+	* Throws Poco::RuntimeException if a module is unable to start
+
+	Parameter:
+	@1 : Name of server instance
+	@2 : Path to FastCraft root directory
+	*/
+	MinecraftServer(string,Poco::Path);
+
+
+	/*
+	* Destructor
+	* Will shutdown server if is still running
+	*/
+	~MinecraftServer();
+
+	
+	/*
+	* Thread main
+	*/
+	void run();
+
+
+	/*
+	* Shuts down server
+	*/
+	void shutdown();
+
+
+	/*
+	* Returns a pointer to the internal PlayerPool instance
+	*/
+	PlayerPool* getPlayerPool();
+
+
+	/*
+	* Returns a pointer to the internal ItemInfornationProvider instance
+	*/
+	ItemInformationProvider* getItemInfoProvider();
+
+
+	/*
+	* Returns the port which is the server is running on
+	*/
+	unsigned short getPort(); 
+	
+	
+	/*
+	* Returns how many players can join the server at the same time
+	*/
+	unsigned short getPlayerSlotCount();
+
+
+	/*
+	* Returns the server description, shown in the server list of the client
+	*/
+	string getServerDescription();
+
+
+	/*
+	* Returns the message of the day 
+	* This will be posted into chat after a player join
+	*/
+	string getServerMOTD();
+
+
+	/*
+	* Returns the server mode
+	* false for survival, true for creative
+	*/
+	bool getServerMode();
+
+
+	/*
+	* Returns the server difficulty 
+	* Constants.h -> FC_DIFFICULTY_
+	*/
+	char getDifficulty();
+
+
+	/*
+	* Returns true if name verification is required 
+	*/
+	bool isOnlineModeActivated();
+
+
+	/*
+	* Returns true if whitelist verification is required
+	*/
+	bool isWhitelistActivated();
+
+
+	/*
+	* Returns the spawn map name
+	*/
+	string getMainWorldName();
+
+
+	/*
+	* Returns true if neather is allowed
+	*/
+	bool isNeatherAllowed(); 
+
+
+	/*
+	* Returns true if peacful/hostile mobs are allowed to spawn
+	*/
+	bool isPeacefulAnimalSpawningActivated();
+	bool isHostileAnimalSpawningActivated();
+
+
+	/*
+	* Returns how many chunks a send around the player (raduis of chunk spawning circle)
+	*/
+	char getViewDistance();
+
+	/*
+	* Returns true if player vs. player actions will affect their health
+	*/
+	bool isPVPActivated();
+
+
+	/*
+	* Returns true if zombe's flymod is allowed
+	*/
+	bool isFlyModAllowed();
+
+
+	/*
+	* Returns maximal moving speed
+	* The highest vanilla running speed is 7.5 km/h
+	*/
+	double getMaxMovementSpeed();
+
+
+	/*
+	* Returns the ServerFull message
+	*/
+	string getServerFullMessage();
+
+
+	/* 
+	* Returns pointer to world by given name
+	* Returns NULL if world wasn't found
+	* Search is case sensitive
+
+	Parameter:
+	@1 : World name to search
+	*/
+	World* getWorldByName(string);
+
+
+	/*
+	* Returns actual in-game time
+	*/
+	Tick getInGameTime();
+
+
+	/*
+	* Returns time in milliseconds since class was created
+	*/
+	Tick getTimestamp();
+
+
+	/*
+	* Generates a new unique ID
+	*/
+	int generateID();
+private:
+	bool Int2Bool(int&);
+	bool parseNodeInt(Poco::AutoPtr<Poco::XML::Document>,string,int&,int,int);
+	bool parseNodeDouble(Poco::AutoPtr<Poco::XML::Document>,string,double&,double,double);
+	bool parseNodeString(Poco::AutoPtr<Poco::XML::Document>,string,string&);
+	void readConfiguration(Poco::Path&);
+};
+#endif
