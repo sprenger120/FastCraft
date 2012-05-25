b915f72946732eeed1d07af6568028fde7e04ff4
Sprenger120 <sprenger120@gmx.de>
2012-05-26 00:00:45 +0200

    Added class for the local instance of an entity in PlayerThreads

---
+++ FastCraft/EntityListEntry.cpp
@@ -0,0 +1,59 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+#include "EntityListEntry.h"
+#include "ItemSlot.h"
+#include "FCRuntimeException.h"
+
+EntityListEntry::EntityListEntry(Entity* pEntity) {
+	_iEntityID = pEntity->getEntityID();
+	Position = pEntity->Coordinates;
+
+
+	if (pEntity->isAlive()) {
+		EntityLiving* pLiving = (EntityLiving*)pEntity;
+		Equipment.resize(5);
+		
+		_iType = pLiving->getType();
+		_fAlive = true;
+
+		for (char x=0;x<=4;x++) {
+			Equipment[x] = new ItemSlot(pEntity->getMinecraftServer()->getItemInfoProvider(),pLiving->getEquipment(x),1);
+		}
+	}else{
+		_fAlive = false;
+	}
+}
+
+EntityListEntry::~EntityListEntry(){
+	if (!Equipment.empty()){
+		for (char x=0;x<=4;x++) {
+			if (Equipment[x] != NULL){delete Equipment[x];}
+		}
+	}
+}
+
+
+bool EntityListEntry::isAlive(){
+	return _fAlive;
+}
+
+char EntityListEntry::getType() {
+	if (!_fAlive) {throw FCRuntimeException("Not alive");}
+	return _iType;
+}
+
+int EntityListEntry::getEntiyID(){
+	return _iEntityID;
+}
---
+++ FastCraft/EntityListEntry.h
@@ -0,0 +1,71 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_ENTITYLISTENTRY
+#define _FASTCRAFTHEADER_ENTITYLISTENTRY
+#include "EntityLiving.h"
+
+/*
+* This is a storage class for all spawned entities in
+* PlayerThreads
+*/
+class MinecraftServer;
+
+class EntityListEntry {
+	int _iEntityID;
+	bool _fAlive;
+	char _iType; /* TypeID of EntityLiving instances */
+public:
+	/*
+	* Constructor
+	
+	Parameter:
+	@1 : Entity instance
+	*/ 
+	EntityListEntry(Entity*);
+
+
+	/*
+	* Destructor
+	*/
+	~EntityListEntry();
+
+
+	/*
+	* Returns true if entity is alive
+	*/
+	bool isAlive();
+
+
+	/*
+	* Returns type of liviing entity
+	* Throws FCRuntimeException if entity isn't alive
+	*/
+	char getType();
+
+
+	/*
+	* Returns EntityID
+	*/
+	int getEntiyID();
+
+
+	/*
+	* Position & Equipment
+	*/
+	EntityCoordinates Position;
+	EquipmentArray Equipment; 
+};
+#endif
