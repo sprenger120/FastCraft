e19e73eec42ab842ce4ae2e5e37a68fd27bb8842
Sprenger120 <sprenger120@gmx.de>
2012-07-12 14:21:11 +0200

    Added PlayerInventory class

---
+++ FastCraft/InventoryPlayer.cpp
@@ -0,0 +1,114 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+#include "InventoryPlayer.h"
+#include "MinecraftServer.h"
+#include "NetworkIn.h"
+#include "NetworkOutRoot.h"
+#include "PlayerThread.h"
+#include <utility>
+
+InventoryPlayer::InventoryPlayer(PlayerThread* pPlayer,MinecraftServer* pMCServer,NetworkIn& rIn) try : 
+	ItemContainer(45,pMCServer,0),
+	_rNetworkIn(rIn)
+{
+	_lowerRange = std::pair<short,short>(36,44);
+	_higherRange = std::pair<short,short>(9,35);
+	_pPreferredRange = &_higherRange;
+	_pPlayer = pPlayer;
+	_iActionBarSelection = 0;
+}catch(FCRuntimeException& ex){
+	ex.rethrow();
+}
+
+string InventoryPlayer::getName() {
+	return string("Player Inventory");
+}
+
+void InventoryPlayer::reset() {
+	clear();
+	_iActionBarSelection = 0;
+}
+
+void InventoryPlayer::readHeldItemChange() {
+	_iActionBarSelection = (char)_rNetworkIn.readShort();
+	if (_iActionBarSelection < 0 || _iActionBarSelection > 8) {_pPlayer->Disconnect("Illegal ActionBarSelection ID");}
+}
+
+void InventoryPlayer::onDisconnect() {
+	reset();
+}
+
+void InventoryPlayer::onLogin() {
+	NetworkOut Out(_pPlayer->getNetworkOutRoot());
+	syncInventory(Out);
+}
+
+ItemSlot* InventoryPlayer::getSelectedSlot() {
+	return _vSlots[36+_iActionBarSelection];
+}
+
+void InventoryPlayer::DecreaseInHandStack() {
+	char iSize = getSelectedSlot()->getStackSize();
+	if (iSize == 1) {
+		getSelectedSlot()->clear();
+	}else{
+		getSelectedSlot()->setStackSize(iSize-1);
+	}
+}
+
+bool InventoryPlayer::isAllowedToPlace(short iSlot,ItemID itemID) {
+	if (iSlot <= 5 && iSlot >= 8){ 
+		if (!_vSlots[iSlot]->isEmpty()) {return false;}
+		if (_vSlots[iSlot]->getStackSize() + 1 > 1){return false;}
+	}
+	switch(iSlot) {
+	case 0:
+		return false;
+	case 5:
+		if (itemID.first != 298 && itemID.first != 302 && 
+			itemID.first != 306 && itemID.first != 310 && 
+			itemID.first != 314)
+		{
+			return false;
+		}
+		return true;
+	case 6:
+		if (itemID.first != 299 && itemID.first != 303 && 
+			itemID.first != 307 && itemID.first != 311 && 
+			itemID.first != 315)
+		{
+			return false;
+		}
+		return true;
+	case 7:
+		if (itemID.first != 300 && itemID.first != 304 && 
+			itemID.first != 308 && itemID.first != 312 && 
+			itemID.first != 316)
+		{
+			return false;
+		}
+		return true;
+	case 8:
+		if (itemID.first != 301 && itemID.first != 305 && 
+			itemID.first != 309 && itemID.first != 313 && 
+			itemID.first != 317)
+		{
+			return false;
+		}
+		return true;
+	default:
+		return true;
+	}
+}
---
+++ FastCraft/InventoryPlayer.h
@@ -0,0 +1,93 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_INVENTORYPLAYER
+#define _FASTCRAFTHEADER_INVENTORYPLAYER
+#include "ItemContainer.h"
+
+class NetworkOutRoot;
+
+class InventoryPlayer : public ItemContainer {
+private:
+	char _iActionBarSelection;
+	PlayerThread* _pPlayer;
+	NetworkIn& _rNetworkIn;
+public:
+	/*
+	* Constructor
+	* Rethrows all InventoryPlayer exceptions
+
+	Parameter:
+	@1 : this pointer of player
+	@2 : Minecraft Server instance
+	@2 : his NetworkIn instance
+	*/
+	InventoryPlayer(PlayerThread*,MinecraftServer*,NetworkIn&);
+
+	
+	/*
+	* Returns Inventory Name
+	*/
+	string getName();
+
+
+	/*
+	* Clears slots and resets variables
+	*/
+	void reset();
+
+
+	/*
+	* Parses the HeldItemChange packet
+	*/
+	void readHeldItemChange();
+
+
+	/*
+	* Call this when the player disconnects
+	*/
+	void onDisconnect();
+
+
+	/*
+	* Call this when the player logs in
+	*/
+	void onLogin();
+
+
+	/*
+	* Decreases stack count of actual held item
+	* Synchronizes with player
+	*/
+	void DecreaseInHandStack();
+
+
+	/*
+	* Returns the actual selected item in action bar
+	*/
+	ItemSlot* getSelectedSlot();
+
+	
+	/*
+	* Checks if you are allowed to place the given item 
+	* in the given slot
+
+	Parameter:
+	@1 : slotID
+	@2 : Item
+	*/
+	bool InventoryPlayer::isAllowedToPlace(short,ItemID);
+};
+#endif
