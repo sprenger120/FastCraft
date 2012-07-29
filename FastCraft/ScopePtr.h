b0bd61d15f420442de1516f23db0a22f5fe46c26
Sprenger120 <sprenger120@gmx.de>
2012-07-29 21:29:01 +0200

    Added ScopePtr and ScopePtrArray

---
+++ FastCraft/ScopePtr.h
@@ -0,0 +1,85 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_SCOPEPTR	
+#define _FASTCRAFTHEADER_SCOPEPTR
+#include "FCRuntimeException.h"
+
+template <typename type>
+class ScopePtr {
+	type* _pPtr;
+public:
+	/*
+	* Constructor
+	* "Takes care" of the given pointer (deletes it on destruction)
+	* Throws FCRuntimeException if pointer is NULL
+
+	Parameter:
+	@1 : Pointer
+	*/
+	ScopePtr(type*);
+
+
+	/*
+	* Constructor
+	* Allocates a new instance of type
+	*/
+	ScopePtr();
+
+
+	/*
+	* Destructor
+	*/
+	~ScopePtr()
+
+
+	/*
+	* Operators
+	*/
+	type* operator->();
+	type& operator*();
+};
+
+
+
+template <typename type>
+ScopePtr<type>::ScopePtr(type* pData) {
+	if (pData == NULL) {throw FCRuntimeException("Nullpointer");}
+	_pPtr = pData;
+}
+
+template <typename type>
+ScopePtr<type>::ScopePtr() {
+	if (pData == NULL) {throw FCRuntimeException("Nullpointer");}
+	_pPtr = new type;
+}
+
+template <typename type>
+ScopePtr<type>::~ScopePtr() {
+	delete _pPtr;
+}
+
+
+template <typename type>
+type* ScopePtr<type>::operator->() {
+	return _pPtr;
+}
+
+
+template <typename type>
+type& ScopePtr<type>::operator*() {
+	return *_pPtr;
+}
+#endif
---
+++ FastCraft/ScopePtrArray.h
@@ -0,0 +1,79 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_SCOPEPTRARRAY
+#define _FASTCRAFTHEADER_SCOPEPTRARRAY
+#include "FCRuntimeException.h"
+
+template <typename type>
+class ScopePtrArray {
+	type* _pData;
+	int _iSize;
+public:
+	/*
+	* Constructor
+	* Throws FCRuntimeException if element count is illegal 
+
+	Parameter
+	@1 : Element count
+	*/
+	ScopePtrArray(int);
+
+
+	/*
+	* Destructor
+	*/
+	~ScopePtrArray();
+
+
+	/*
+	* Get operator
+	* Throws FCRuntimeException if index is out of range
+	*/ 
+	type& operator[](int);
+
+
+	/*
+	* Returns internal pointer to array
+	*/
+	type* getPtr();
+};
+
+
+template <typename type>
+ScopePtrArray<type>::ScopePtrArray(int iSize) {
+	if (iSize <= 0) {throw FCRuntimeException("Illegal size");}
+	_pData = new type[iSize];
+	_iSize = iSize;
+}
+
+
+template <typename type>
+ScopePtrArray<type>::~ScopePtrArray() {
+	delete [] _pData;
+}
+
+
+template <typename type>
+type& ScopePtrArray<type>::operator[](int index) {
+	if (index < 0 || index > _iSize -1) {throw FCRuntimeException("Illegal index");}
+	return _pData[index];
+}
+
+template <typename type>
+type* ScopePtrArray<type>::getPtr() {
+	return _pData;
+}
+#endif
