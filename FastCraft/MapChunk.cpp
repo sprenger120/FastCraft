fe3971c18c703509b8c79b8a49d76443969642e8
sprenger120 <sprenger120@gmx.de>
2012-08-20 13:19:49 +0200

    Reimplemented MapChunk class

---
+++ FastCraft/MapChunk.cpp
@@ -0,0 +1,350 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+#include "MapChunk.h"
+#include "FCRuntimeException.h"
+#include <Poco\ScopedLock.h>
+#include "ChunkMath.h"
+#include "NetworkOut.h"
+#include <sstream>
+#include <iostream>
+#include <cstring>
+#include "NBTAll.h"
+#include <bitset>
+
+
+using Poco::Mutex;
+using std::stringstream;
+using Poco::DeflatingOutputStream;
+
+MapChunk::MapChunk(int X,int Z) {
+	_iX = X;
+	_iZ = Z;
+	_pData = NULL;
+	_pBiomeArray = NULL;
+	_pLevelCompound = NULL;
+	_iLastAccess = _timestamp.epochTime();
+}
+
+MapChunk::~MapChunk() {
+	if (_pData) {delete _pData;}
+	std::cout<<"chunk saving is not implemented\n";
+}
+
+int MapChunk::getX() {
+	return _iX;
+}
+
+int MapChunk::getZ() {
+	return _iZ;
+}
+
+bool MapChunk::isLoaded() {
+	return _pData != NULL;
+}
+
+time_t MapChunk::getLastAccess() {
+	return _iLastAccess;
+}
+
+void MapChunk::setBlock(ItemID& item,char X,short Y,char Z) {
+	if (!isLoaded()) {throw FCRuntimeException("Not loaded");}
+	if (X < 0 || Z < 0 || X > 15 || Z > 15 || Y < 0 || Y > 255) {throw FCRuntimeException("Illegal coordinates");}
+
+	Mutex::ScopedLock scopelock(_Mutex);
+	_iLastAccess = _timestamp.epochTime();
+
+	char iPtrArrIndex = getSegment(Y>>4);
+	int index = ChunkMath::toIndex(X,Y - (Y>>4),Z);
+	
+	_vpBlocks[iPtrArrIndex].first[index] = (char)item.first;
+	setNibble(index,item.second,_vpMetadata[iPtrArrIndex]);
+}
+
+ItemID MapChunk::getBlock(char X,short Y,char Z) {
+	if (!isLoaded()) {throw FCRuntimeException("Not loaded");}
+	if (X < 0 || Z < 0 || X > 15 || Z > 15 || Y < 0 || Y > 255) {throw FCRuntimeException("Illegal coordinates");}
+	
+	_iLastAccess = _timestamp.epochTime();
+
+	char iPtrArrIndex = getSegment(Y>>4);
+	int index = ChunkMath::toIndex(X,Y - (Y>>4),Z);
+	
+	return ItemID(_vpBlocks[iPtrArrIndex].first[index],getNibble(index,_vpMetadata[iPtrArrIndex]));
+}
+
+void MapChunk::setBlockLight(char iLvl,char X,short Y,char Z) {
+	if (!isLoaded()) {throw FCRuntimeException("Not loaded");}
+	if (X < 0 || Z < 0 || X > 15 || Z > 15 || Y < 0 || Y > 255) {throw FCRuntimeException("Illegal coordinates");}
+
+	Mutex::ScopedLock scopelock(_Mutex);
+	_iLastAccess = _timestamp.epochTime();
+
+	char iPtrArrIndex = getSegment(Y>>4);
+	int index = ChunkMath::toIndex(X,Y - (Y>>4),Z);
+	
+	setNibble(index,iLvl,_vpBlockLight[iPtrArrIndex]);
+}
+
+char MapChunk::getBlockLight(char X,short Y,char Z) {
+	if (!isLoaded()) {throw FCRuntimeException("Not loaded");}
+	if (X < 0 || Z < 0 || X > 15 || Z > 15 || Y < 0 || Y > 255) {throw FCRuntimeException("Illegal coordinates");}
+	
+	_iLastAccess = _timestamp.epochTime();
+
+	char iPtrArrIndex = getSegment(Y>>4);
+	int index = ChunkMath::toIndex(X,Y - (Y>>4),Z);
+	
+	return getNibble(index,_vpBlockLight[iPtrArrIndex]);
+}
+
+void MapChunk::setSkyLight(char iLvl,char X,short Y,char Z) {
+	if (!isLoaded()) {throw FCRuntimeException("Not loaded");}
+	if (X < 0 || Z < 0 || X > 15 || Z > 15 || Y < 0 || Y > 255) {throw FCRuntimeException("Illegal coordinates");}
+
+	Mutex::ScopedLock scopelock(_Mutex);
+	_iLastAccess = _timestamp.epochTime();
+
+	char iPtrArrIndex = getSegment(Y>>4);
+	int index = ChunkMath::toIndex(X,Y - (Y>>4),Z);
+	
+	setNibble(index,iLvl,_vpSkyLight[iPtrArrIndex]);
+}
+
+char MapChunk::getSkyLight(char X,short Y,char Z) {
+	if (!isLoaded()) {throw FCRuntimeException("Not loaded");}
+	if (X < 0 || Z < 0 || X > 15 || Z > 15 || Y < 0 || Y > 255) {throw FCRuntimeException("Illegal coordinates");}
+	
+	_iLastAccess = _timestamp.epochTime();
+
+	char iPtrArrIndex = getSegment(Y>>4);
+	int index = ChunkMath::toIndex(X,Y - (Y>>4),Z);
+	
+	return getNibble(index,_vpSkyLight[iPtrArrIndex]);
+}
+
+void MapChunk::setNibble(int iBlockOffset,char iNewData,char* apArray) {
+	if (iBlockOffset < 0) {throw FCRuntimeException("Illegal block offset");}
+	int iNibbleOffset = iBlockOffset/2;
+
+	if (iBlockOffset%2) { //Higher 4-bits
+		apArray[iNibbleOffset] |= iNewData<<4;
+	}else{//Lower 4-bits
+		apArray[iNibbleOffset] |= iNewData & 0xF;
+	}
+}
+
+char MapChunk::getNibble(int iBlockOffset,char* apArray) {
+	if (iBlockOffset < 0) {throw FCRuntimeException("Illegal block offset");}
+	int iNibbleOffset = iBlockOffset/2;
+
+	if (iBlockOffset%2) { //Higher 4-bits
+		return apArray[iNibbleOffset]>>4;
+	}else{//Lower 4-bits
+		return apArray[iNibbleOffset]&0xF;
+	}
+}
+
+void MapChunk::send(NetworkOut& Out) {
+	if (!isLoaded()) {throw FCRuntimeException("Not loaded");}
+
+	try {
+		char iSize = _vpBlocks.size();
+		if (iSize == 0) {return;}
+
+
+		std::stringstream _stringStrm;
+		Poco::DeflatingOutputStream _deflatingStrm(_stringStrm,Poco::DeflatingStreamBuf::STREAM_ZLIB,-1);
+		
+		short iPrimaryBitmap = packData(_deflatingStrm);
+		_deflatingStrm.flush();
+
+		/* Pack */
+		Out.addByte(0x33);
+		Out.addInt(_iX);
+		Out.addInt(_iZ);
+		Out.addBool(true);
+
+		Out.addShort(iPrimaryBitmap);
+		Out.addShort(0);
+
+		Out.addInt(_stringStrm.str().length());
+		Out.getStr().append(_stringStrm.str());
+
+		Out.Finalize(FC_QUEUE_LOW);
+	}catch(FCRuntimeException& ex) {
+		ex.rethrow();
+	}
+}
+
+short MapChunk::packData(Poco::DeflatingOutputStream& rStrm) {
+	if (!isLoaded()) {throw FCRuntimeException("Not loaded");}
+	char iSize = _vpBlocks.size();
+	if (iSize == 0) {return 0;}
+
+	std::bitset<16> primaryBitset;
+	vector<pair<char*,short>> vArrays;
+
+	primaryBitset.reset();
+	vArrays.resize(iSize*4);
+
+	for(char x = 0;x<=iSize-1;x++) {
+		primaryBitset.set(_vpBlocks[x].second,true);
+		vArrays[x]			 = std::make_pair(_vpBlocks[x].first,4096);
+		vArrays[x+iSize]	 = std::make_pair(_vpMetadata[x]	,2048);
+		vArrays[x+(iSize*2)] = std::make_pair(_vpBlockLight[x]	,2048);
+		vArrays[x+(iSize*3)] = std::make_pair(_vpSkyLight[x]	,2048);
+	}
+	for (short x = 0;x<=(iSize*4)-1;x++) {
+		rStrm.write(vArrays[x].first,vArrays[x].second);
+	}
+	rStrm.write(_pBiomeArray,256);
+
+	return (short)primaryBitset.to_ulong();
+}
+
+void MapChunk::unload(WorldFileHandler* p) {
+	throw FCRuntimeException("not implemented");
+}
+
+void MapChunk::save(WorldFileHandler* p) {
+	throw FCRuntimeException("not implemented");
+}
+
+void MapChunk::alloc() {
+	if (isLoaded()) {return;}
+	NBTTagByte* Byte;
+	NBTTagInt* Int;
+	NBTTagByteArray* ByteArray;
+
+	_pData = _pLevelCompound = new NBTTagCompound("Level");
+
+	_pData->addSubElement(Byte = new NBTTagByte("TerrainPopulated"));
+	Byte->getDataRef() = 1;
+
+	_pData->addSubElement(Int = new NBTTagInt("xPos"));
+	Int->getDataRef() = _iX;
+
+	_pData->addSubElement(Int = new NBTTagInt("zPos"));
+	Int->getDataRef() = _iZ;
+
+	_pData->addSubElement(ByteArray = new NBTTagByteArray("Biomes",_pBiomeArray = new char[256],256));
+	memset(_pBiomeArray,0,256);
+
+	_pData->addSubElement(new NBTTagList("Entities",FC_NBT_TYPE_COMPOUND));
+	_pData->addSubElement(new NBTTagList("Sections",FC_NBT_TYPE_COMPOUND));
+	_pData->addSubElement(new NBTTagList("TitleEntities",FC_NBT_TYPE_COMPOUND));
+	_pData->addSubElement(new NBTTagIntArray("HeightMap",256));
+}
+
+void MapChunk::updatePointerArrays() {
+	_vpBlocks.clear();
+	_vpMetadata.clear();
+	_vpBlockLight.clear();
+	_vpSkyLight.clear();
+
+	NBTTagList* pSections = dynamic_cast<NBTTagList*>(_pLevelCompound->getElementByName("Sections"));
+	char iSize = 0;
+	if ((iSize = pSections->size()) == 0) {return;}
+
+	_vpBlocks.resize(iSize);
+	_vpMetadata.resize(iSize);
+	_vpBlockLight.resize(iSize);
+	_vpSkyLight.resize(iSize);
+
+	NBTTagCompound* pChunkPart;
+	char iPos;
+
+	for(char x = 0;x<=iSize-1;x++) {
+		pChunkPart = dynamic_cast<NBTTagCompound*>((*pSections)[x]);
+		iPos = dynamic_cast<NBTTagByte*>(pChunkPart->getElementByName("Y"))->getDataRef();
+
+		_vpBlocks[x]		= std::make_pair(((NBTTagByteArray*)pChunkPart->getElementByName("Blocks"))->getDataRef(),iPos);
+		_vpMetadata[x]		= ((NBTTagByteArray*)pChunkPart->getElementByName("Data"))->getDataRef();
+		_vpBlockLight[x]	= ((NBTTagByteArray*)pChunkPart->getElementByName("BlockLight"))->getDataRef();
+		_vpSkyLight[x]		= ((NBTTagByteArray*)pChunkPart->getElementByName("SkyLight"))->getDataRef();
+	}
+	_pBiomeArray = ((NBTTagByteArray*)_pLevelCompound->getElementByName("Biomes"))->getDataRef();
+}
+
+void MapChunk::addSegment() {
+	if (_vpBlocks.size() == 16) {return;} //Maxmal amount of chunks
+
+	char iY = -1;
+	if (!_vpBlocks.empty()) {
+		for (char x=0;x<=_vpBlocks.size()-1;x++) {
+			if (_vpBlocks[x].second > iY) {iY = _vpBlocks[x].second;}
+		}
+		iY++;
+	}else{
+		iY = 0;
+	}
+
+	char* pBlocks	= new char[4096];
+	char* pMetadata = new char[2048]; 
+	char* pBlockLight = new char[2048]; 
+	char* pSkyLight = new char[2048]; 
+
+	memset(pBlocks,0,4096);
+	memset(pMetadata,0,2048);
+	memset(pBlockLight,0,2048);
+	memset(pSkyLight,0,2048);
+
+	
+	//Add to pointer arrays
+	_vpBlocks.push_back(std::make_pair(pBlocks,iY));
+	_vpMetadata.push_back(pMetadata);
+	_vpBlockLight.push_back(pBlockLight);
+	_vpSkyLight.push_back(pSkyLight);
+
+
+	//Create chunk part NBT structure
+	NBTTagCompound* pChunkPart = new NBTTagCompound("");
+	NBTTagByte* pByte;
+
+	pChunkPart->addSubElement(pByte = new NBTTagByte("Y"));
+	pByte->getDataRef() = iY;
+
+	pChunkPart->addSubElement(new NBTTagByteArray("Blocks",pBlocks,4096));
+	pChunkPart->addSubElement(new NBTTagByteArray("Data",pMetadata,2048));
+	pChunkPart->addSubElement(new NBTTagByteArray("BlockLight",pBlockLight,2048));
+	pChunkPart->addSubElement(new NBTTagByteArray("SkyLight",pSkyLight,2048));
+
+
+	//Add to Sections
+	NBTTagList* pSections = dynamic_cast<NBTTagList*>(_pLevelCompound->getElementByName("Sections"));
+	pSections->addSubElement(pChunkPart);
+}
+
+char MapChunk::getSegment(char iSearchedSegment) {
+	Poco::Mutex::ScopedLock lock(_segmentCreation);
+
+	if (_vpBlocks.empty()) {
+		addSegment();
+		return 0;
+	}else{
+		for (char x=0;x<=_vpBlocks.size()-1;x++) {
+			if (_vpBlocks[x].second == iSearchedSegment) {
+				return x;
+			}
+		}
+		addSegment();
+		return char(_vpBlocks.size()-1);
+	}
+	return 0; //Compiler warning fix
+}
+
+bool MapChunk::isEmpty() { 
+	return _vpBlocks.empty();
+}
---
+++ FastCraft/MapChunk.h
@@ -0,0 +1,190 @@
+/*
+FastCraft - Minecraft SMP Server
+Copyright (C) 2011 - 2012 Michael Albrecht aka Sprenger120
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#ifndef _FASTCRAFTHEADER_MAPCHUNK
+#define _FASTCRAFTHEADER_MAPCHUNK
+#include "ItemID.h"
+#include <Poco\Mutex.h>
+#include <Poco\Timestamp.h>
+#include "Types.h"
+#include <vector>
+#include <utility>
+#include <Poco\DeflatingStream.h>
+
+using Poco::Mutex;
+using std::vector;
+using std::pair;
+class NetworkOut;
+class NBTTagCompound;
+class WorldFileHandler;
+
+class MapChunk {
+	friend class World;
+	friend class WorldFileHandler;
+private:
+	int _iX;
+	int _iZ;
+
+	time_t _iLastAccess;
+	Mutex _Mutex;
+	Mutex _segmentCreation;
+
+	Poco::Timestamp _timestamp;
+
+	NBTTagCompound* _pData;
+	NBTTagCompound* _pLevelCompound;
+
+	vector<pair<char*,char>> _vpBlocks;
+	vector<char*> _vpMetadata;
+	vector<char*> _vpBlockLight;
+	vector<char*> _vpSkyLight;
+	char* _pBiomeArray;
+public:
+	/*
+	* Constructor
+	
+	Parmeter:
+	@1 : Chunks X coordinate
+	@2 : Chunks Z coordinate
+	*/ 
+	MapChunk(int,int);
+
+
+	/*
+	* Destructor
+	* Saves/deallocates chunk data
+	*/
+	~MapChunk();
+
+
+	/* 
+	* Coordinate accessators
+	*/
+	int getX();
+	int getZ();
+
+
+	/*
+	* Accessators for data arrays
+	* All functions require chunk coordinates (x/z: 0-15, y 0-127)
+	* All functions throw FCRuntimeException if given coordiantes 
+	  are invalid
+	* All functions will throw FCRuntimeException if chunk is not loaded
+	* No functions will check block existance
+	
+	get... Parameters:
+	@1 : X coordinate
+	@2 : Y coordiante
+	@3 : Z coordiante
+
+	set... Parameters:
+	@1 : Data to set
+	@2 : X coordinate
+	@3 : Y coordiante
+	@4 : Z coordiante
+	*/
+	void setBlock(ItemID&,char,short,char);
+	ItemID getBlock(char,short,char);
+		
+	void setBlockLight(char,char,short,char);
+	char getBlockLight(char,short,char);
+
+	void setSkyLight(char,char,short,char);
+	char getSkyLight(char,short,char);
+
+
+	/*
+	* Appends a MapChunk packet to given NetworkOut
+	* Throws FCRuntimeException if chunk isn't loaded
+	*/
+	void send(NetworkOut&);
+
+
+	/*
+	* Returns true if chunk was loaded into memory
+	*/ 
+	bool isLoaded();
+
+
+	/*
+	* Unloads chunk from memory
+	* Does nothing if is already unloaded
+
+	Parameter:
+	@1 : WorldFileHandler instance to handle saving
+	*/ 
+	void unload(WorldFileHandler*);
+
+
+	/*
+	* Saves chunk to given WorldFileHandler instance
+	*/
+	void save(WorldFileHandler*);
+
+
+	/*
+	* Returns tick timestamp of last access
+	*/ 
+	time_t getLastAccess();
+
+	
+	/*
+	* Returns true if no chunk sections were allocated
+	*/
+	bool isEmpty();
+
+	
+	/*
+	* Adds chunk data to given DeflatingOutputStream 
+	* Returns primary bitmap
+	*/
+	short packData(Poco::DeflatingOutputStream&);
+private:
+	/* 
+	* Throws FCRuntimeException if block offset is invalid
+
+	Parameter:
+	@1 : Block offset
+	@2 : new data
+	@3 : array where the data has to be set
+	*/
+	void setNibble(int,char,char*);
+	char getNibble(int,char*);
+
+
+	/*
+	* Allocates Chunks NBT structure
+	*/
+	void alloc();
+
+
+	/*
+	* Updates pointer storages
+	*/
+	void updatePointerArrays();
+
+
+	/*
+	* Adds a new 16x16x16 segment of chunk space
+	*/ 
+	void addSegment(); //Adds a new segment ontop of all others
+
+	/*
+	* Returns segment array index
+	* If not, it generates a new one
+	*/
+	char getSegment(char);
+};
+#endif
